fb950d42a92621dca5db8b9f8e8dc34a684b1dd3
thivatm <thivagartm@gmail.com>
2018-04-24 10:50:21 +0530

    first commmit

---
+++ server.js
@@ -0,0 +1,32 @@
+//Import the necessary modules
+var express =  require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+//Array of users and connection
+users = [];
+conn = [];
+
+server.listen(process.env.PORT || 3000);
+console.log("running...");
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    conn.push(socket);
+    console.log('Connected: %s sockets' + conn.length);
+
+    socket.on('disconnect', function(data){
+        conn.splice(conn.indexOf(socket), 1);
+        console.log('Disconnected: Sockets Connected ' + conn.length);
+    });
+
+    //Sending messages
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg:data});
+    });
+});
