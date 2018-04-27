c503d1153e9347b8e67fc8479056d4ae571c5e21
Thivagar <thivagartm@gmail.com>
2018-04-27 10:25:17 +0530

    Updated

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ io.sockets.on('connection', function(socket){
     console.log('Connected: %s sockets' + conn.length);
 
     socket.on('disconnect', function(data){
+        
+        users.splice(users.indexOf(socket.userName), 1);
+        //update users
+        io.sockets.emit('get users', users);
+
         conn.splice(conn.indexOf(socket), 1);
         console.log('Disconnected: Sockets Connected ' + conn.length);
     });
@@ -27,6 +32,16 @@ io.sockets.on('connection', function(socket){
     //Sending messages
     socket.on('send message', function(data){
         console.log(data);
-        io.sockets.emit('new message', {msg:data});
+        io.sockets.emit('new message', {msg:data, user: socket.userName});
     });
+
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.userName = data;
+        users.push(socket.userName);
+        //update users
+        io.sockets.emit('get users', users);
+    });
+
+
 });
